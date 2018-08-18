bf46e7e7c3fada8daf1901fcab6793bbc285af02
keelii <keeliizhou@gmail.com>
2018-08-18 09:13:30 +0800

    add comment function.

--- pythonx/javascript_snippets.py
+++ pythonx/javascript_snippets.py
@@ -3,6 +3,7 @@
 """
 Helper utilities to format javascript snippets.
 """
+import re
 
 ALWAYS = 'always'
 NEVER = 'never'
@@ -46,3 +47,26 @@ def keyword_spacing(snip):
     else:
         ret = ''
     return ret
+
+def comment(snip, COMMENT_SIGN):
+    lines = snip.v.text.split('\n')[:-1]
+    first_line = lines[0]
+    spaces = ''
+
+    # Get the first non-empty line
+    for idx, l in enumerate(lines):
+        if l.strip() != '':
+            first_line = lines[idx]
+            sp = re.search(r'\s+', first_line)
+            spaces = sp.group()
+
+    if first_line.strip().startswith(COMMENT_SIGN):
+        result = [f'{line.replace(COMMENT_SIGN, "", 1)}' if line.strip() else line for line in lines]
+    else:
+        result = [f'{spaces}{COMMENT_SIGN}{line[len(spaces):]}' if line.strip() else line for line in lines ]
+
+    if result:
+        return '\n'.join(result)
+    else:
+        return ''
+
